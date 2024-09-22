12916a9236c7ebc4982c4b2b00577daf4d272138
ItsOnlyBinary <git@itsonlybinary.com>
2024-09-22 23:14:55 +0100

    [WIP]

--- test/commands/handler.test.js
+++ test/commands/handler.test.js
@@ -2,9 +2,9 @@
 
 /* globals describe, it */
 /* eslint-disable no-unused-expressions */
+const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
 const ircLineParser = require('../../src/irclineparser');
 const NetworkInfo = require('../../src/networkinfo');
 const IrcCommandHandler = require('../../src/commands/handler');
@@ -14,46 +14,54 @@ function newMockClient() {
 
     const mockClient = {
         network: new NetworkInfo(),
-        emit: sinon.stub(),
     };
 
     const handler = new IrcCommandHandler(mockClient);
 
-    handler.emit = (...args) => {
-        if (args[0] === 'privmsg') {
-            return;
-        }
-        console.log('emit:', args);
-    };
+    // handler.emit = (...args) => {
+    //     if (args[0] === 'privmsg') {
+    //         return;
+    //     }
+    //     console.log('emit:', JSON.stringify(args, null, 2));
+    // };
 
     mockClient.command_handler = handler;
-    mockClient.test_spies = {
-        emit: sinon.spy(handler.emit),
+    mockClient.spies = {
+        emit: sinon.spy(handler, 'emit'),
     };
     return mockClient;
 }
 
 describe('src/handler.js', function() {
     describe('BATCH support', function() {
-        // it('should correctly handle simple batches', function() {
-        //     const lines = [
-        //         ':irc.host BATCH +yXNAbvnRHTRBv netsplit irc.hub other.host',
-        //         '@batch=yXNAbvnRHTRBv :aji!a@a QUIT :irc.hub other.host',
-        //         '@batch=yXNAbvnRHTRBv :nenolod!a@a QUIT :irc.hub other.host',
-        //         ':nick!user@host PRIVMSG #channel :This is not in batch, so processed immediately',
-        //         '@batch=yXNAbvnRHTRBv :jilles!a@a QUIT :irc.hub other.host',
-        //         ':irc.host BATCH -yXNAbvnRHTRBv',
-        //     ];
-        //     const mockClient = newMockClient();
+        it('should correctly handle simple batches', function() {
+            const lines = [
+                ':irc.host BATCH +yXNAbvnRHTRBv netsplit irc.hub other.host',
+                '@batch=yXNAbvnRHTRBv :aji!a@a QUIT :irc.hub other.host',
+                '@batch=yXNAbvnRHTRBv :nenolod!a@a QUIT :irc.hub other.host',
+                ':nick!user@host PRIVMSG #channel :This is not in batch, so processed immediately',
+                '@batch=yXNAbvnRHTRBv :jilles!a@a QUIT :irc.hub other.host',
+                ':irc.host BATCH -yXNAbvnRHTRBv',
+            ];
+            const mockClient = newMockClient();
 
-        //     lines.forEach((line) => {
-        //         const ircLine = ircLineParser(line);
-        //         mockClient.command_handler.dispatch(ircLine);
-        //     });
-        //     expect(mockClient.test_spies.emit).to.have.been.calledWith(['batch end']);
+            lines.forEach((line) => {
+                const ircLine = ircLineParser(line);
+                mockClient.command_handler.dispatch(ircLine);
+            });
 
-        //     console.log('mock', mockClient);
-        // });
+            expect(mockClient.spies.emit).to.have.been.called;
+
+            expect(mockClient.spies.emit).to.have.been.calledWithMatch(
+                'batch end',
+                sinon.match({
+                    id: 'yXNAbvnRHTRBv',
+                    type: 'netsplit',
+                    params: sinon.match((val) => Array.isArray(val) && val.length === 2),
+                    commands: sinon.match((val) => Array.isArray(val) && val.length === 3),
+                }),
+            );
+        });
 
         it('should correctly handle interleaving batches', function() {
             const lines = [
@@ -67,15 +75,72 @@ describe('src/handler.js', function() {
                 '@batch=2 :nick!user@host PRIVMSG #channel :Message 5',
                 ':irc.host BATCH -2',
             ];
-            const mockClient = newMockClient();
 
+            const mockClient = newMockClient();
             lines.forEach((line) => {
                 const ircLine = ircLineParser(line);
                 mockClient.command_handler.dispatch(ircLine);
             });
-            expect(mockClient.test_spies.emit).to.have.been.calledWith(['batch end']);
 
-            console.log('mock', mockClient);
+            expect(mockClient.spies.emit).to.have.been.called;
+
+            expect(mockClient.spies.emit).to.have.been.calledWithMatch(
+                'batch end',
+                sinon.match({
+                    id: '1',
+                    type: 'example.com/foo',
+                    params: [],
+                    commands: sinon.match((val) => Array.isArray(val) && val.length === 3),
+                }),
+            );
+
+            expect(mockClient.spies.emit).to.have.been.calledWithMatch(
+                'batch end',
+                sinon.match({
+                    id: '2',
+                    type: 'example.com/foo',
+                    params: [],
+                    commands: sinon.match((val) => Array.isArray(val) && val.length === 2),
+                }),
+            );
         });
+
+        // it('should correctly handle nested batches', function() {
+        //     const lines = [
+        //         ':irc.host BATCH +outer example.com/foo',
+        //         '@batch=outer :irc.host BATCH +inner example.com/bar',
+        //         '@batch=inner :nick!user@host PRIVMSG #channel :Hi',
+        //         '@batch=outer :irc.host BATCH -inner',
+        //         ':irc.host BATCH -outer',
+        //     ];
+
+        //     const mockClient = newMockClient();
+        //     lines.forEach((line) => {
+        //         const ircLine = ircLineParser(line);
+        //         mockClient.command_handler.dispatch(ircLine);
+        //     });
+
+        //     expect(mockClient.spies.emit).to.have.been.called;
+
+        //     expect(mockClient.spies.emit).to.have.been.calledWithMatch(
+        //         'batch end',
+        //         sinon.match({
+        //             id: 'outer',
+        //             type: 'example.com/foo',
+        //             params: [],
+        //             commands: [sinon.match((val) => Array.isArray(val) && val.length === 1)],
+        //         }),
+        //     );
+
+        //     expect(mockClient.spies.emit).to.have.been.calledWithMatch(
+        //         'batch end',
+        //         sinon.match({
+        //             id: 'inner',
+        //             type: 'example.com/foo',
+        //             params: [],
+        //             commands: sinon.match((val) => Array.isArray(val) && val.length === 1),
+        //         }),
+        //     );
+        // });
     });
 });
